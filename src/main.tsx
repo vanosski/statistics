708b99a7cd529e928bcaf412ed66e4375728ee12
vanosski <parolharyan30@gmail.com>
2026-09-02 21:36:18 +0530

    Rewrite authentication using Google Identity SDK directly to hide Supabase redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
 
 import { AuthProvider } from './contexts/AuthContext'
 
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </StrictMode>,
 )

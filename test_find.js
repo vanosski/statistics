3a498382598cb63ac673896c49bf446dece69340
vanosski <parolharyan30@gmail.com>
2026-09-07 22:20:18 +0530

    chore: clean up old raw data and utility scripts

--- test_find.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-const players = JSON.parse(fs.readFileSync('src/data/public_players.json', 'utf8'));
-const kingdoms = JSON.parse(fs.readFileSync('src/data/kingdoms.json', 'utf8'));
-
-const kd = kingdoms.find(k => k.server === 'K162');
-const wocLeader = players.find(p => p.server === kd.server && p.is_woc_leader) || null;
-
-console.log(wocLeader);
--- test_spread.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const playersData = JSON.parse(fs.readFileSync('src/data/players.json'));
-const detData = [{ name: '(277)X-', server: 'K27', lethal: 26 }];
-
-const sum = playersData.find(p => p.name === '(277)X-');
-const det = detData[0];
-
-const merged = { ...sum, ...det };
-console.log(merged.is_woc_leader);
